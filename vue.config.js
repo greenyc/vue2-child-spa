525709b6a9141c69641134c41266be86d35386bb
Ciaran Green <ciaran.green@hermes-europe.co.uk>
2022-10-24 13:46:29 +0100

    config swapped to single-spa example

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
-const webpack = require("webpack")
+
 module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    extract: false,
-  },
-  configureWebpack: (config) => {
-    config.plugins.push(
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 1,
-      })
-    )
-    config.output.filename = "[name].js"
-  },
+  filenameHashing: false
 });
