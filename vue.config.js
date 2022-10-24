c88bca1604f217d5f22ddaed5cb765692290a887
Ciaran Green <ciaran.green@hermes-europe.co.uk>
2022-10-24 12:39:43 +0100

    added main entry point, made it chunk to 1 chunk at dist/app.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack")
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  css: {
+    extract: false,
+  },
+  configureWebpack: (config) => {
+    config.plugins.push(
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      })
+    )
+    config.output.filename = "[name].js"
+  },
+});
